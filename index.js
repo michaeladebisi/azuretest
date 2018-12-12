81168a126aa0c19cea192f8e9dc273da6ecb28cc
michaeladebisi <michael.adebisi@centrica.com>
2018-12-12 17:58:02 +0000

    Adding carol

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello World!</h1>");
+    response.write("<h1>Hello Carol!</h1>");
     response.end("<h1>Hello MIKE!</h1>");
 
 });
